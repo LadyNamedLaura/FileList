0ca1a8646f5fce216deffa9ae9b1a5a0f841ba61
Simon Peeters <peeters.simon@gmail.com>
2011-10-09 12:14:56 +0200

    update i18n

--- FileList.i18n.php
+++ FileList.i18n.php
@@ -15,7 +15,7 @@ $messages['en'] = array(
   'fl_empty_list'              => 'This page doesn\'t have any files yet.',
   'fl_upload_file_anonymously' => 'Upload file anonymously',
   'fl_empty_file'              => 'Select a file please',
-  'fl_remove_confirm'          => 'You are about to delete the file "$1" along with all of its history.',
+  'fl_remove_confirm'          => 'You are about to delete the file "$1" along with all of its history.', //like 'filedelete-intro' but plaintext
   'fl_add'                     => 'Add new file',
   // credits
   'fl_credits_desc'            => 'Generates a dynamic file-list at the insertion of <code>&lt;filelist /&gt;</code>',
@@ -26,6 +26,8 @@ $messages['nl'] = array(
   'fl_empty_list'              => 'Deze pagina heeft nog geen bestanden.',
   'fl_upload_file_anonymously' => 'Bestand anoniem uploaden',
   'fl_empty_file'              => 'U hebt nog geen bestand ingevoegd',
+  'fl_remove_confirm'          => 'U staat op het punt om het bestand "$1" te verwijderen, inclusief alle eerdere versies.',
+  'fl_add'                     => 'Nieuw bestand toevoegen',
   // credits
   'fl_credits_desc'            => 'Genereert dynamische file-lijst met uploadformulier bij het invoeren van <code>&lt;filelist /&gt;</code>',
 );
@@ -35,6 +37,8 @@ $messages['fr'] = array(
   'fl_empty_list'              => 'Cette page n\'a pas de files.',
   'fl_upload_file_anonymously' => 'Téléverser un fichier anonyme',
   'fl_empty_file'              => 'Sélectionnez un fichier s\'il vous plaît',
+  'fl_remove_confirm'          => 'Vous êtes sur le point de supprimer $1 ainsi que tout son historique.',
+  'fl_add'                     => 'Ajouter un fichier',
   // credits
   'fl_credits_desc'            => 'Génère une fichier-liste dynamique à l\'insertion de <code>&lt;filelist /&gt;</code>',
 );
@@ -44,6 +48,8 @@ $messages['sv'] = array(
   'fl_empty_list'              => 'Sidan har inga filor än.',
   'fl_upload_file_anonymously' => 'Ladda up en fil anonym',
   'fl_empty_file'              => 'Va vänligen att selectera en fil',
+  'fl_remove_confirm'          => 'Du håller på att radera filen $1 tillsammans med hela dess historik.',
+  'fl_add'                     => 'Lägg till ny fil',
   // credits
   'fl_credits_desc'            => 'Genererar en dynamisk file-list med uploadform när du skriver <code>&lt;filelist /&gt;</code>',
 );
